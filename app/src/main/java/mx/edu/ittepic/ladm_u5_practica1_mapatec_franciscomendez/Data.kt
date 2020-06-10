44d06c52263d97b70138b3b56229819a97b39443
FranciscoMendez007 <55217668+FranciscoMendez007@users.noreply.github.com>
2020-06-09 21:50:10 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u5_practica1_mapatec_franciscomendez/Data.kt
@@ -0,0 +1,32 @@
+package mx.edu.ittepic.ladm_u5_practica1_mapatec_franciscomendez
+
+import com.google.firebase.firestore.GeoPoint
+
+class Data {
+    var nombre : String = ""
+    var posicion1 : GeoPoint = GeoPoint(0.0,0.0)
+    var posicion2 : GeoPoint = GeoPoint(0.0,0.0)
+
+    override fun toString(): String {
+        return nombre+"\n"+posicion1.latitude+","+posicion1.longitude+"\n"+
+                posicion2.latitude+","+posicion2.longitude
+    }
+
+    fun estoyEn(posicionActual: GeoPoint):Boolean{
+        //logica es similar a la clase figura geometrica de canvas y concretamente
+        //estoyEn es muy muy similar a estaEnArea
+        if (posicionActual.latitude>=posicion1.latitude && posicionActual.longitude <= posicion2.latitude){
+            if (invertir(posicionActual.longitude) >= invertir(posicion1.longitude)
+                && invertir(posicionActual.longitude) <= invertir(posicion2.longitude)){
+
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun invertir(valor:Double):Double{
+        return valor * -1
+    }
+
+}
